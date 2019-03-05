d552d392f477b41b9a8b214fa5524a2d3d3c45f9
fureweb-com <fureweb.com@gmail.com>
2019-03-06 01:29:55 +0900

    WA-2 수정: WeatherService 응답 방식 변경
    - response 객체를 더이상 인자로 받지 않는다.
    - 에러가 발생하는 경우 errorMessage 키에 메시지를 전달한다.
    - 모든 응답은 컨트롤러에서 작성하도록 한다.
    - 향후 에러용 별도의 클래스를 만들어 처리할 것
    - ref #2

--- api/services/WeatherService.js
+++ api/services/WeatherService.js
@@ -1,8 +1,9 @@
 module.exports = {
-  getWeather (req, res) {
+  getWeather (req) {
     const { query: { lat, lng } } = req
 
-    if (!lat || !lng) return res.badRequest({ errorMessage: '위도와 경도는 필수 요소입니다.' })
+    // TODO: validation에 대한 처리를 향후 별도의 Error 핸들링용 객체를 이용하는 방식으로 변경할 것
+    if (!lat || !lng) return { errorMessage: '위도와 경도는 필수 요소입니다.' }
     // TODO: lat, lng에 대한 range validation 필요
 
     const weather = {
