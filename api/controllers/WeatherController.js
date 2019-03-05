eddd671c305ac3c7f4bdd21481c128c60278929f
fureweb-com <fureweb.com@gmail.com>
2019-03-06 01:14:04 +0900

    WA-2 수정: WeatherController의 getWeather 메서드 로직 변경
    - 서비스로부터 얻은 순수한 데이터를 의미있는 변수에 담아서 직접 응답하도록 변경한다.
    - ref #2

--- api/controllers/WeatherController.js
+++ api/controllers/WeatherController.js
@@ -2,6 +2,7 @@ const { weatherservice: WeatherService } = global.sails.services
 
 module.exports = {
   getWeather (req, res) {
-    return WeatherService.getWeather(req, res)
+    const weather = WeatherService.getWeather(req, res)
+    return res.json(weather)
   }
 }
