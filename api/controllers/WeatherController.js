687f76da96c9373d4c5241be76f7ad310868e17d
fureweb-com <fureweb.com@gmail.com>
2019-03-06 01:32:02 +0900

    WA-2 수정: WeatherController 응답 작성방식 변경
    - 모든 응답은 컨트롤러에서 작성한다.
    - 서비스에는 request 객체만 전달한다.
    - 향후 errorMessage 키 대신 별도 에러 인스턴스 타입체크를 통해 400+ 응답을 작성하도록 변경한다.
    - this commit resolves #2

--- api/controllers/WeatherController.js
+++ api/controllers/WeatherController.js
@@ -2,7 +2,9 @@ const { weatherservice: WeatherService } = global.sails.services
 
 module.exports = {
   getWeather (req, res) {
-    const weather = WeatherService.getWeather(req, res)
-    return res.json(weather)
+    const response = WeatherService.getWeather(req)
+
+    if (response.errorMessage) return res.badRequest(response)
+    else return res.json(response)
   }
 }
