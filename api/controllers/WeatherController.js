1e8bf5bfa755f7e7ae64b2315543aa28a27587b3
fureweb-com <fureweb.com@gmail.com>
2019-03-07 12:30:44 +0900

    WA-5 수정: WeatherController에서 요청과 응답을 책임지도록 수정
    - 서비스와는 데이터만으로 통신하도록 변경
    - lat와 lng는 숫자로 캐스팅하여 전달
    - ref #5

--- api/controllers/WeatherController.js
+++ api/controllers/WeatherController.js
@@ -2,9 +2,13 @@ const { weatherservice: WeatherService } = global.sails.services
 
 module.exports = {
   getWeather (req, res) {
-    const response = WeatherService.getWeather(req)
+    const { query: { lat, lng } } = req
 
-    if (response.errorMessage) return res.badRequest(response)
-    else return res.json(response)
+    // TODO: validation에 대한 처리를 향후 별도의 Error 핸들링용 객체를 이용하는 방식으로 변경할 것
+    if (!lat || !lng) return res.badRequest({ errorMessage: '위도와 경도는 필수 요소입니다.' })
+
+    const response = WeatherService.getWeather(+lat, +lng)
+
+    return res.json(response)
   }
 }
