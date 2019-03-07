279ca744fe539bafe3e5b86a2e1aa30f1343f3b4
fureweb-com <fureweb.com@gmail.com>
2019-03-07 12:32:29 +0900

    WA-5 수정: WeatherService의 getWeather 메서드 파라미터 수정
    - lat와 lng는 숫자로 전달받는게 보장되도록 한다.
    - 전달받은 인자를 임시 응답 내용으로 작성한다.
    - this commit resolves #5

--- api/services/WeatherService.js
+++ api/services/WeatherService.js
@@ -1,12 +1,12 @@
 module.exports = {
-  getWeather (req) {
-    const { query: { lat, lng } } = req
-
-    // TODO: validation에 대한 처리를 향후 별도의 Error 핸들링용 객체를 이용하는 방식으로 변경할 것
-    if (!lat || !lng) return { errorMessage: '위도와 경도는 필수 요소입니다.' }
-    // TODO: lat, lng에 대한 range validation 필요
+  getWeather (lat, lng) {
+    // TODO: lat와 lng 범위를 정규화 처리하여 가장 가까운 곳으로 매칭되도록 한다.
 
     const weather = {
+      // 임시 응답 작성
+      lat,
+      lng,
+
       // TODO: 날씨 관련 정보, 최저기온, 최고기온, 강수확률, 강수량, PM2.5, PM10, O3, NO2, CO, SO2 등등 위경도를 통해 얻을 수 있는 모든 정보를 응답 메시지로 작성한다.
     }
 
